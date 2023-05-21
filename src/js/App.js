f2848b3b137287085e2a9fad1c6c86387e0b256e
bravedevrabbit <williamsmoisha@gmail.com>
2023-05-22 00:06:27 +0300

    added screens to views folder

--- src/js/App.js
+++ src/js/App.js
@@ -6,19 +6,22 @@ import {
   Route
 } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Login from "./views/login";
+import Register from "./views/register";
+import Settings from "./views/settings";
 
 export default function App() {
   return(
     <Router>
       <Navbar/>
-          <div className='content-wrapper'>
-        <Routes>
-            <Route path="/settings" element={<HomeView/>}/>
-            <Route path="/login" element={<LoginView/>}/>
-            <Route path="/register" element={<HomeView/>}/>
-            <Route path="/" element={<HomeView/>}/>
-        </Routes>
-          </div>
+        <div className='content-wrapper'>
+          <Routes>
+              <Route path="/" element={<HomeView/>} exact="true"/>
+              <Route path="/settings" element={<Settings/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/register" element={<Register/>}/>
+          </Routes>
+        </div>
     </Router>
   )
 }
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,23 +1,27 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function Navbar(){
+  const navigate = useNavigate() 
+  
+
   return(
     <div className="chat-navbar">
         <nav className="chat-navbar-inner">
           <div className="chat-navbar-inner-left">
+            <button onClick={() => navigate(-1)} className="btn btn-outline-primarry">Back</button>
             <Link to="/settings" className="btn btn-outline-success ml-2">Settings</Link>
           </div>
           <div className="chat-navbar-inner-right">
             <span className="logged-in-user">Hi User</span>
-            <Link
-              to = '/register'
-              onClick={() => {}}
-              className="btn btn-sm btn-outline-danger ml-2">Register</Link>
-            <Link
-              to = '/login'
-              onClick={() => {}}
-              className="btn btn-sm btn-outline-success ml-2">Login</Link>
+            <button
+              
+              onClick={() => navigate('/register')}
+              className="btn  btn-outline-danger ml-2">Register</button>
+            <button
+              
+              onClick={() => navigate('/login')}
+              className="btn  btn-outline-success ml-2">Login</button>
           </div>
         </nav>
       </div>
